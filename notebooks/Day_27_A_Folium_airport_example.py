11ce48464d642a73b044af1646c4df3b57ef3152
Raymond Yee <raymond.yee@gmail.com>
2014-04-26 21:11:57 -0700

    Adding note about the need to run the master branch of folium to do lines in Day_27_A_Folium_airport_example.ipynb

--- notebooks/Day_27_A_Folium_airport_example.py
+++ notebooks/Day_27_A_Folium_airport_example.py
@@ -6,6 +6,14 @@
 # I recommend using Folium (see [Day_23_B_folium-ipython.ipynb](Day_23_B_folium-ipython.ipynb)) instead of trying to get Google Maps working in the Notebook for the course.  Should be easier.
 # 
 # This notebook is a demonstration of plotting airport data we get from the project on airport data.
+# 
+# **Note**:  As of 2014.04.26, to get the lines working, you need to be running the master branch of folium.  To install it, you might need to uninstall folium first:
+# 
+#     pip uninstall folium
+#     
+# and then install the master branch:
+# 
+#     pip install git+git://github.com/wrobstory/folium.git
 
 # <codecell>
 
@@ -57,6 +65,37 @@ for airport in islice(airports,None):
     airport_map.simple_marker([lat,lon],popup=label)
 
 inline_map(airport_map)
+
+# <headingcell level=1>
+
+# Lines
+
+# <codecell>
+
+# https://github.com/wrobstory/folium/blob/master/examples/line_example.py
+
+coordinates = [
+    [  42.3581    ,  -71.0636    ],
+    [  42.82995815,  -74.78991444],
+    [  43.17929819,  -78.56603306],
+    [  43.40320216,  -82.37774519],
+    [  43.49975489,  -86.20965845],
+    [  43.46811941,  -90.04569087],
+    [  43.30857071,  -93.86961818],
+    [  43.02248456,  -97.66563267],
+    [  42.61228259, -101.41886832],
+    [  42.08133868, -105.11585198],
+    [  41.4338549 , -108.74485069],
+    [  40.67471747, -112.29609954],
+    [  39.8093434 , -115.76190821],
+    [  38.84352776, -119.13665678],
+    [  37.7833    , -122.4167    ]]
+
+
+# Create the map and add the line
+m = folium.Map(location=[41.9, -97.3], zoom_start=4)
+m.line(coordinates, line_color='#FF0000', line_weight=5)
+inline_map(m)
 
 # <codecell>
 
